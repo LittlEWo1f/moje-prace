c87a6a1dba7e5eb85a8f50b60371773538e07705
Maja Dojwa <98595942+MagicMadnessMoon@users.noreply.github.com>
2022-02-04 14:15:07 +0100

    Add files via upload

---
+++ zad04-02/script.js
@@ -0,0 +1,11 @@
+function fun() {
+alert ("This is an alert dialog box");
+}
+
+function policz(a,b){
+alert("= "+(a+b));
+return a+b;
+}
+function podpowiedz(){
+alert("Czy nie wstyd Ci to sprawdzać" );
+}
